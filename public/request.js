0947e5cb4259e227bbd97af7376ffb94c45eb54b
Reem Qais <reemq86@hotmail.com>
2017-03-30 12:02:12 +0300

    create makeRequest function in request.js

--- public/request.js
+++ public/request.js
@@ -1,3 +1,19 @@
 // receives calls from listener.js and onload
 // sends xhr requests to router.js
 // receives responses from xhr callback
+
+function makeRequest(method, url, callback) {
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.onreadystatechange === 4 && xhr.status === 200) {
+      callback(null, JSON.parse(xhr.responseText));
+    } else {
+      callback(new Error("Status Code: ", xhr.status))
+    }
+  }
+  xhr.open(method, url);
+  xhr.send();
+
+return {makeRequest: makeRequest}
+}
