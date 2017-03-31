a71c44d8bee9bde3419a2ebaa44c9900de8b59c6
Reem Qais <reemq86@hotmail.com>
2017-03-31 10:33:51 +0300

    Replaced table names with restaurants and ratings in db_build.sql

--- database/db_build.sql
+++ database/db_build.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 DROP TABLE IF EXISTS restaurantInfo cascade;
 
-CREATE TABLE restaurant_info (
+CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE,
   cuisine VARCHAR(100) NOT NULL,
@@ -11,22 +11,22 @@ CREATE TABLE restaurant_info (
   price_range INT NOT NULL
 );
 
-INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
+INSERT INTO restaurants (name, cuisine, address, open_hours, price_range)
 VALUES ('Casa Nova', 'Falafel', 'Al Bishara St 4, Nazareth, 17100', 'Monday-Sunday: 08:00-20:00', 2);
 
-INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
+INSERT INTO restaurants (name, cuisine, address, open_hours, price_range)
 VALUES ('Tishreen', 'Asian', '24 Marys Road, Nazareth, 14500', 'Monday-Sunday: 13:00-22:00', 5);
 
-INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
+INSERT INTO restaurants (name, cuisine, address, open_hours, price_range)
 VALUES ('Cheeky Shwarma', 'Fast Food', '6 Shwarma Street', 'Monday-Friday: 16:00-00:00', 5);
 
 COMMIT;
 
 BEGIN;
 
-DROP TABLE IF EXISTS restaurant_ratings cascade;
+DROP TABLE IF EXISTS ratings cascade;
 
-CREATE TABLE restaurant_ratings (
+CREATE TABLE ratings (
   id SERIAL PRIMARY KEY,
   restaurant_id INT NOT NULL,
   rating INT NOT NULL,
@@ -34,13 +34,13 @@ CREATE TABLE restaurant_ratings (
   reviewer_name TEXT
 );
 
-INSERT INTO restaurant_ratings (restaurant_id, rating, review, reviewer_name)
+INSERT INTO ratings (restaurant_id, rating, review, reviewer_name)
 VALUES (1, 4, 'Great falafel, noisy parrot', 'King Edgar');
 
-INSERT INTO restaurant_ratings (restaurant_id, rating, review, reviewer_name)
+INSERT INTO ratings (restaurant_id, rating, review, reviewer_name)
 VALUES (2, 2, 'Average food. Good service', 'Ravenous Reem');
 
-INSERT INTO restaurant_ratings (restaurant_id, rating, review, reviewer_name)
+INSERT INTO ratings (restaurant_id, rating, review, reviewer_name)
 VALUES (3, 5, 'The cheekiest shwarma I ever ate, seriously!', 'ShwarMacintosh Helper');
 
 COMMIT;
