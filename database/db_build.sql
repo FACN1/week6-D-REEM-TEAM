e30eecf0228a121ceb1e5a1b5e2f67832eff374e
Mario <mario.saliba98@gmail.com>
2017-03-31 10:17:41 +0300

    refactoring db_build.sql

--- database/db_build.sql
+++ database/db_build.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS restaurantInfo cascade;
+DROP TABLE IF EXISTS restaurantInfo, restaurant_ratings cascade;
 
 CREATE TABLE restaurant_info (
   id SERIAL PRIMARY KEY,
@@ -11,20 +11,6 @@ CREATE TABLE restaurant_info (
   price_range INT NOT NULL
 );
 
-INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
-VALUES ('Casa Nova', 'Falafel', 'Al Bishara St 4, Nazareth, 17100', 'Monday-Sunday: 08:00-20:00', 2);
-
-INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
-VALUES ('Tishreen', 'Asian', '24 Marys Road, Nazareth, 14500', 'Monday-Sunday: 13:00-22:00', 5);
-
-INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
-VALUES ('Cheeky Shwarma', 'Fast Food', '6 Shwarma Street', 'Monday-Friday: 16:00-00:00', 5);
-
-COMMIT;
-
-BEGIN;
-
-DROP TABLE IF EXISTS restaurant_ratings cascade;
 
 CREATE TABLE restaurant_ratings (
   id SERIAL PRIMARY KEY,
@@ -34,6 +20,19 @@ CREATE TABLE restaurant_ratings (
   reviewer_name TEXT
 );
 
+/* INSERTING into the first table restaurant_info */
+
+INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
+VALUES ('Casa Nova', 'Falafel', 'Al Bishara St 4, Nazareth, 17100', 'Monday-Sunday: 08:00-20:00', 2);
+
+INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
+VALUES ('Tishreen', 'Asian', '24 Marys Road, Nazareth, 14500', 'Monday-Sunday: 13:00-22:00', 5);
+
+INSERT INTO restaurant_info (name, cuisine, address, open_hours, price_range)
+VALUES ('Cheeky Shwarma', 'Fast Food', '6 Shwarma Street', 'Monday-Friday: 16:00-00:00', 5);
+
+/*INSTERTING into the second table restaurant_ratings */
+
 INSERT INTO restaurant_ratings (restaurant_id, rating, review, reviewer_name)
 VALUES (1, 4, 'Great falafel, noisy parrot', 'King Edgar');
 
@@ -42,5 +41,6 @@ VALUES (2, 2, 'Average food. Good service', 'Ravenous Reem');
 
 INSERT INTO restaurant_ratings (restaurant_id, rating, review, reviewer_name)
 VALUES (3, 5, 'The cheekiest shwarma I ever ate, seriously!', 'ShwarMacintosh Helper');
+
 
 COMMIT;
